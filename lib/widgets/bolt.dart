fe56c9ca185d6e9e8c5db5a588d1e2fc227ca230
Mateo <itj.17131@gmail.com>
2023-01-31 20:53:58 -0600

    Cambios extras

--- lib/widgets/bolt.dart
+++ lib/widgets/bolt.dart
@@ -1,5 +1,5 @@
+import 'package:actividad_2/widgets/text_open2.dart';
 import 'package:flutter/material.dart';
-
 class BoltCar extends StatelessWidget {
   const BoltCar({super.key});
 
@@ -8,7 +8,9 @@ class BoltCar extends StatelessWidget {
     return Row( 
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        IconButton(onPressed: () {}, icon: Icon(Icons.bolt, color: Colors.white))
+        IconButton(onPressed: () {}, icon: Icon(Icons.bolt, color: Colors.white)),
+        SizedBox(width: 100),
+        TextOpen2()
       ]);
     
   }
